067e964442bdb6de0199f07d235d74f6ff449080
Vanik <vanik_888@mail.ru>
2014-04-16 01:48:33 +0400

    added adapter

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,26 +1,20 @@
 package com.example.app;
 
 import android.app.Activity;
-import android.content.ContentValues;
-import android.content.Context;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
-
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
     public final static String myLogger = "myLogger";
+    private ListView lvItems;
+    private MyAdapter myAdapter;
+    private ArrayList<StudentDataSet> sds;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,16 +24,28 @@ public class MainActivity extends Activity {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        final Uri STUDENTS_URI = Uri
-                .parse("content://com.example.app.provider/students");
-        Cursor cursor = getContentResolver().query(STUDENTS_URI, null, null,
-                null, null);
-//        startManagingCursor(cursor);
-        int nameIndex = cursor.getColumnIndex("name");
-        int surnameIndex = cursor.getColumnIndex("surname");
-        int ageIndex = cursor.getColumnIndex("age");
-        if (cursor.moveToFirst()) {
-            Log.d(myLogger, "name = " +  cursor.getString(nameIndex));
+    }
+    public void onSendClick(View v) {
+        if(v.getId() == R.id.btnSend) {
+            final Uri STUDENTS_URI = Uri
+                    .parse("content://com.example.app.provider/students");
+            Cursor cursor = getContentResolver().query(STUDENTS_URI, null, null,
+                    null, null);
+            int nameIndex = cursor.getColumnIndex("name");
+            int surnameIndex = cursor.getColumnIndex("surname");
+            int ageIndex = cursor.getColumnIndex("age");
+            sds = new ArrayList<StudentDataSet>();
+            if (cursor.moveToFirst()) {
+                String name = cursor.getString(nameIndex);
+                String surname = cursor.getString(surnameIndex);
+                int age = cursor.getInt(ageIndex);
+                sds.add(new StudentDataSet(name, surname, age));
+                Log.d(myLogger, "name = " + name + " surname = " + surname + " age = " + age);
+            }
+            myAdapter = new MyAdapter(this, sds);
+            lvItems = (ListView) findViewById(R.id.lvItems);
+            lvItems.setAdapter(myAdapter);
+
         }
     }
 
